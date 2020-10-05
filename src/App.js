9ec031d0cd7f80b0dd1053d86e2e6c6d037b1fd1
achyuth reddy <ncsachyuth@gmail.com>
2020-10-06 01:10:27 +0530

    token stored in the state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from "./components/Login"
 import { getTokenFromUrl } from './spotify';
 
 function App() {
 
+  const [token, setToken] = useState(null)
+
+
+
   useEffect(() => {
-    const token = getTokenFromUrl()
+    const hash = getTokenFromUrl()
+    window.location.hash = "";
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+    }
     console.log(" i have a token ", token);
   },[])
 
   return (
     <div className="app">
+
+      {
+        token ? (
+          <h2> Ia m logged in </h2>
+        ) : (
+            <Login />
+        )
+      }
       
       {/* spotify logo */}
       {/* login with spotify */}
-      <Login />
+      {/* <Login /> */}
 
 
     </div>
