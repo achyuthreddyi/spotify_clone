609bdd8a9daaa962f6ac7eaf7691bd93b98e3b82
achyuth reddy <ncsachyuth@gmail.com>
2020-10-05 23:27:08 +0530

    login component implemented

---
+++ src/components/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+function index() {
+    return (
+        <div className="login">
+            <img
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                alt=""
+            />
+            <a> Login with Spotify </a>
+        </div>
+    )
+}
+
+export default index
