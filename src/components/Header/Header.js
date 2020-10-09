bf940ba5459a1171c1bf9e4e66d28c9b40af79ae
Achyuth Reddy <ncsachyuth@gmail.com>
2020-10-10 01:26:24 +0530

    header is perfect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,14 @@ import React from 'react'
 import "./style.css"
 import SearchIcon from "@material-ui/icons/Search"
 import { Avatar } from "@material-ui/core"
+import { useDataLayerValue } from '../../context/DataLayer'
 
 
 function Header() {
+
+    const [ { user }, dispatch ] = useDataLayerValue()
+
+
     return (
         <div className = "header">
            <div className= "header__left">
@@ -16,8 +21,8 @@ function Header() {
            </div>
 
            <div className= "header__right">
-               <Avatar src="" alt="" />
-               <h4> Achyuth Reddy</h4>
+               <Avatar src={user?.images[0]?.url} alt= {user?.display_name} />
+           <h4>  {user?.display_name} </h4>
 
            </div>
         </div>
