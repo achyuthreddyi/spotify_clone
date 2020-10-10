98fa1da9b1f0e9fe4ba65a67c6566150eb986aa9
Achyuth Reddy <ncsachyuth@gmail.com>
2020-10-10 12:57:24 +0530

    update

--- src/pages/Player/Player.js
+++ src/pages/Player/Player.js
@@ -22,7 +22,9 @@ function Player({ spotify }) {
                 {/*  Body  */}
             </div>            
             {/* footer */}
-            <Footer />
+            <Footer 
+            spotify
+            />
         </div>
     )
 }
